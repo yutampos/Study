b43c85cda1ffb9a50ab03e3b50815e9fd4dbdfc3
yutampos <100180010+yutampos@users.noreply.github.com>
2022-03-27 18:49:22 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,25 +9,43 @@ import java.util.Scanner;
 public class Main {
 	public static void main(String [] args) {
 		
-		/* 課題１*/
+		kadai1();
+		
+		kadai2();
+		
+		kadai3();
+		
+		kadai4();
+		
+		kadai5();
+		
+		kadai6();
+		
+	}
+	public static void kadai1() {
+		/* 課題１　*/
 		String greeting;
 		greeting="HelloWorld";
 		System.out.println(greeting);
-		
-		/* 課題２*/
+		}
+	
+	public static void kadai2() {
+		/* 課題２　if文で変数sumに１を１０回足した後、出力しています*/
 		int sum=0;
 		for (int i = 0; i < 10; i++) {
 			sum = +i;
 			}
 		System.out.println(sum);
-		
-		/* 課題３*/
+	}
+	public static void kadai3() {
+		/* 課題３　plusDateとDayOfWeekを使い3年後の曜日を出力しています*/
 		LocalDateTime LDT=LocalDateTime.now();
 		LocalDateTime plusDate =LDT.plusYears(3);
 		DayOfWeek  dw= plusDate.getDayOfWeek();
 		System.out.println(dw);
-		
-		/*  課題４　trueOrFalseメソッドに乱数0or1を渡し、if文で条件に沿った文字列を返している*/
+	}
+	public static void kadai4() {
+		/*  課題４　trueOrFalseメソッドに乱数0or1を渡し、if文の条件に沿って真か偽を返しています*/
 		TrueOrFalse TOF =new TrueOrFalse();
 		
 		Random random=new Random();
@@ -36,9 +54,10 @@ public class Main {
 		String result=TOF.trueOrFalse(Num);
 		
 		System.out.println(result);
-		
-		/** 課題５　Personクラスのフィールド、personData（List型）に各苗字を代入している 
-		 * 最後配列数を取得しfor文で全要素を出力 */
+	}
+	public static void kadai5() {
+		/** 課題５　Personクラスのフィールド、personData（List型）に各苗字を代入しています
+		 * 最後配列数を取得しfor文で全要素を出力しています */
 		Person person=new Person();
 		
 		System.out.println("名前を入力してください");
@@ -57,14 +76,12 @@ public class Main {
 			System.out.println(Data.get(i));
 		}
 		
-		
-		/*課題6 例外を起こしてみました。処理文は念の為コメントアウト*/
+	}
+	public static void kadai6() {
+		/*課題6 例外を起こしてみました。*/
 		//String age ="26";
 		//age="26"/34;
 		//System.out.println(age);
-		
-		
-		
 		
 	}
 }
